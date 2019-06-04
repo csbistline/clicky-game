d400b7046a9b649c47c76965faed166295265116
Chris Bistline <chris.bistline@gmail.com>
2019-06-04 12:59:22 -0500

    randomizes logos on refresh, needs to do with each click

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-white-50 p-3 mt-2">
+        <footer className="bg-dark text-white-50 p-3 mt-4">
             Baseball Logo Memory Game | Created by Chris Bistline 2019
         </footer>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,14 +3,37 @@ import Container from 'react-bootstrap/Container'
 import Logo from './Logo'
 
 const Main = (props) => {
+
+    let logosArr = (props.logos);
+
+    const randomizeLogos = () => {
+        logosArr.sort((a, b) => {
+            return 0.5 - Math.random()
+        });
+        console.log(logosArr);
+    }
+
+    const pickLogo = id => {
+        console.log(id);
+    }
+
+    randomizeLogos()
+
     return (
-        <Container className="d-flex justify-content-around">
-            {props.logos.map(logo =>
-                <Logo
-                    id={logo.id}
+        <Container className="d-flex flex-wrap justify-content-around">
+            {logosArr.map(logo =>
+                <div
+                    className="m-2"
                     key={logo.id}
-                    image={logo.image}
-                />
+                    onClick={() => pickLogo(logo.id)}
+                >
+                    <Logo
+                        id={logo.id}
+                        key={logo.id}
+                        image={logo.image}
+
+                    />
+                </div>
             )}
         </Container>
     );
